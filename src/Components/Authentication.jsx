ba4df063417c7a4408dc8ddae14e21d7cd4bb506
Prashanth Naroju <prashanth.naro@gmail.com>
2024-03-25 16:51:33 +0530

    create UI for Login popup

---
+++ src/Components/Authentication.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Authentication = ({handleClose}) => {
+    const [newAccount,setNewAccount]=useState(true)
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    
+
+    const close=()=>{
+            handleClose() 
+    }
+
+    const handleAccountStatus=()=>{
+        setNewAccount(!newAccount)
+    }
+
+  return (
+    <div className='fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center ' >
+      <div className='relative bg-white p-4 m-2 rounded-lg text-center w-1/3'>
+        <h1 className='text-2xl font-bold'>Signup to Idonutcode</h1>
+        <div >
+            <button  onClick={close}>
+            <span 
+            className='absolute -top-2 -right-2'  
+            >
+            <svg width="32px" height="32px" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.24000000000000005"></g><g id="SVGRepo_iconCarrier"> <path d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z" fill="#ee3a55"></path> </g></svg>
+            </span>
+            </button>
+            
+        </div>
+        <div className='m-4 p-2'>
+        <form action="">
+            <input 
+            type="email" 
+            placeholder='email'
+            className='placeholder:text-slate-500 w-full text-md ring-2 p-1 m-3 rounded-sm ring-offset focus:ring-2 focus:ring-green-300 focus:ring-offset ring-blue-300'
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            /> <br />
+            <input 
+            type="password"  
+            placeholder='password' 
+            className='placeholder:text-slate-500 w-full text-md ring-2 p-1 m-3 rounded-sm ring-offset focus:ring-2 focus:ring-green-300 focus:ring-offset ring-blue-300'
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            /> <br />
+            <div>
+                {newAccount?<div>
+            <button className='bg-green-300 p-2 m-2 rounded-lg text-white shadow-sm font-bold hover:bg-green-500'>Login</button> <br />
+            <p>New User? <span className='text-blue-400 cursor-pointer' onClick={handleAccountStatus}>click here</span></p>
+            </div>:<div>
+                <button
+                className='bg-green-300 p-2 m-2 rounded-lg text-white shadow-sm font-bold hover:bg-green-500'
+                >
+                    Signup</button> <br />
+                <p>existing user? <span className='text-blue-400 cursor-pointer' onClick={handleAccountStatus}>click here</span></p>
+            </div>}
+            </div>
+            
+            
+        </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Authentication
