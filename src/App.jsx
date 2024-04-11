67f6e9fd65ae091b585280b551887d6ccdb88b5e
Prashanth Naroju <prashanth.naro@gmail.com>
2024-04-11 14:51:05 +0530

    update the app.jsx to view settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import './App.css'
 import LandingBanner from './Components/LandingBanner'
 import Navbar from './Components/Navbar'
 import Profile from './Components/Profile'
+import Settings from './Components/Settings Components/Settings'
 
 
 function App() {
   
 
   return (
-    <>
-     <div className='bg-slate-50'>
+    
+     <div className='bg-slate-50 '>
       <Navbar /> 
+      <Settings  />
      </div>
-    </>
+ 
   )
 }
 
