9167050f1d645b2e3c1d9d447d02aba18802a36c
Prashanth Naroju <prashanth.naro@gmail.com>
2024-04-23 14:33:06 +0530

    create a courses folder and add main Courses.jsx is created in it

---
+++ src/Components/Courses/Courses.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Courses = () => {
+  return (
+    <div>
+      This is Main parent courses component all the children will be added here.
+    </div>
+  )
+}
+
+export default Courses
