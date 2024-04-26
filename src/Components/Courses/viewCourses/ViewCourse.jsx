69a52aa0bea78e21bdd1d9940608e33a91271dd9
Prashanth Naroju <prashanth.naro@gmail.com>
2024-04-26 13:17:52 +0530

    create and add new viewcourse folder and component

---
+++ src/Components/Courses/viewCourses/ViewCourse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewCourse = () => {
+  return (
+    <div>
+      this is view course component which will be dipslayed when cliked on course from the parent course
+    </div>
+  )
+}
+
+export default ViewCourse
