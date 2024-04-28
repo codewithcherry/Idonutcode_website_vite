351b5347bf20768645ce4f52a45d17c249634ff0
Prashanth Naroju <prashanth.naro@gmail.com>
2024-04-28 13:37:23 +0530

    setup Routes and Route for navbar main components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import './App.css'
 import Courses from './Components/Courses/Courses'
 import ViewCourse from './Components/Courses/viewCourses/ViewCourse'
 import Home from './Components/Home/Home'
-
+import Profile from './Components/Profile'
 import Navbar from './Components/Navbar'
+import Settings from './Components/Settings Components/Settings'
+
+import { Routes,Route, Outlet } from 'react-router-dom'
 
 
 
@@ -14,9 +17,17 @@ function App() {
     
      <div className='bg-slate-50 '>
       <Navbar /> 
-      {/* <Home /> */}
-      {/* <Courses /> */}
-      <ViewCourse />
+
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/home' element={ <Home />} />
+        <Route path='/courses' element={<Courses />} >
+           
+        </Route>
+        <Route path='/profile' element={ <Profile />} />
+        <Route path='/settings' element={ <Settings />} />
+      </Routes>
+      
      </div>
  
   )
