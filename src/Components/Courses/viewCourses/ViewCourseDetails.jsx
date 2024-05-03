48df1034183270c91b8a9835ef036fcd6f70cd07
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-03 21:49:18 +0530

    style the details bar

--- src/Components/Courses/viewCourses/ViewCourseDetails.jsx
+++ src/Components/Courses/viewCourses/ViewCourseDetails.jsx
@@ -5,10 +5,10 @@ const ViewCourseDetails = () => {
     <div>
       <div className='flex w-full '>
                     <ul className='flex justify-around w-full text-left text-xl '>
-                        <li className='text-left font-medium text-slate-600'>About</li>
-                        <li className='text-left font-medium text-slate-600'>Certificaion</li>
-                        <li className='text-left font-medium text-slate-600'>Q&A</li>
-                        <li className='text-left font-medium text-slate-600'>Notes</li>
+                        <li className='text-left font-medium text-slate-600 hover:border-b-2 hover:border-pink-500 p-2 hover:inset-2'>About</li>
+                        <li className='text-left font-medium text-slate-600 hover:border-b-2 hover:border-pink-500 p-2'>Certificaion</li>
+                        <li className='text-left font-medium text-slate-600 hover:border-b-2 hover:border-pink-500 p-2'>Q&A</li>
+                        <li className='text-left font-medium text-slate-600 hover:border-b-2 hover:border-pink-500 p-2'>Notes</li>
                     </ul>
     </div>
     </div>
