f4e704fff1ecddf58eca3d8661998830bca132b7
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-06 14:22:12 +0530

    change the edit button text color to slate-600

--- src/Components/Settings Components/SettingsSkillLanguages.jsx
+++ src/Components/Settings Components/SettingsSkillLanguages.jsx
@@ -37,7 +37,7 @@ const SettingsSkillLanguages = () => {
         <p className='text-sm text-red-500 text-center p-2 '>You can add and edit the skills and languages by clicking edit</p>
       </div>
       <div className='relative'>
-      {edit &&  <button className='flex p-2 m-2 absolute top-2 right-4 z-[5] bg-slate-300 rounded-2xl font-medium' 
+      {edit &&  <button className='flex p-2 m-2 absolute top-2 right-4 z-[5] bg-slate-300 text-slate-600 rounded-2xl font-medium' 
             onClick={()=>setEdit(false)}>
                 <span> 
               <svg width="24px" height="24px" viewBox="-5.28 -5.28 34.56 34.56" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20.1497 7.93997L8.27971 19.81C7.21971 20.88 4.04971 21.3699 3.27971 20.6599C2.50971 19.9499 3.06969 16.78 4.12969 15.71L15.9997 3.84C16.5478 3.31801 17.2783 3.03097 18.0351 3.04019C18.7919 3.04942 19.5151 3.35418 20.0503 3.88938C20.5855 4.42457 20.8903 5.14781 20.8995 5.90463C20.9088 6.66146 20.6217 7.39189 20.0997 7.93997H20.1497Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M21 21H12" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
