9699c73354a28639f04152f985c35dd75eaabddc
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-10 21:22:26 +0530

    add the tailwind container style to the website

---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <div  className='container'>
+      This is the page for currating all the blogs of idonutcode for coding related topics
+    </div>
+  )
+}
+
+export default Blogs
