7d04f09a9dc02bfb35f65aeb78117b251affbf3a
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-20 15:53:57 +0530

    add new title and no show blogs p on main component

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
 const Blogs = () => {
+
+  const blogs={}
   return (
     <div  className='h-screen'>
+      <div className='text-2xl text-left font-medium'>
+        <h1 className='m-4 text-black'>
+          Blogs on Coding
+        </h1>
+      </div>
       <div>
-        <div className='text-2xl text-slate-600 text-center font-medium'>Blogs</div>
+        <div>
+          {
+            blogs? <p className='text-xl text-center font-medium text-slate-600 bg-white p-2 m-2 rounded-md shadow-sm'>No blogs to show</p>:"blogs will be shown here"
+          }
+        </div>
       </div>
     </div>
   )
