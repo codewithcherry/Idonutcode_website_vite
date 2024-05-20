0e2fc63885eae88a7fcc778475aeae3dbab34238
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-20 16:17:15 +0530

    added teh blogs arrray of objects

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -2,7 +2,44 @@ import React from 'react'
 
 const Blogs = () => {
 
-  const blogs={}
+  const blogs=[
+    {
+      title:"How to host a Next.js app in Production on an Ubuntu VPS",
+      description:"Learn how to deploy a Next.js application in a production environment on an Ubuntu VPS with this comprehensive guide. Follow step-by-step instructions to set up NGINX, PM2, and configure your Next.js app for seamless hosting.",
+      author:"Jhon Doe",
+      date:"March 23, 2024"
+    },
+    {
+      title:"How to host a Next.js app in Production on an Ubuntu VPS",
+      description:"Learn how to deploy a Next.js application in a production environment on an Ubuntu VPS with this comprehensive guide. Follow step-by-step instructions to set up NGINX, PM2, and configure your Next.js app for seamless hosting.",
+      author:"Jhon Doe",
+      date:"March 23, 2024"
+    },
+    {
+      title:"How to host a Next.js app in Production on an Ubuntu VPS",
+      description:"Learn how to deploy a Next.js application in a production environment on an Ubuntu VPS with this comprehensive guide. Follow step-by-step instructions to set up NGINX, PM2, and configure your Next.js app for seamless hosting.",
+      author:"Jhon Doe",
+      date:"March 23, 2024"
+    },
+    {
+      title:"How to host a Next.js app in Production on an Ubuntu VPS",
+      description:"Learn how to deploy a Next.js application in a production environment on an Ubuntu VPS with this comprehensive guide. Follow step-by-step instructions to set up NGINX, PM2, and configure your Next.js app for seamless hosting.",
+      author:"Jhon Doe",
+      date:"March 23, 2024"
+    },
+    {
+      title:"How to host a Next.js app in Production on an Ubuntu VPS",
+      description:"Learn how to deploy a Next.js application in a production environment on an Ubuntu VPS with this comprehensive guide. Follow step-by-step instructions to set up NGINX, PM2, and configure your Next.js app for seamless hosting.",
+      author:"Jhon Doe",
+      date:"March 23, 2024"
+    },
+    {
+      title:"How to host a Next.js app in Production on an Ubuntu VPS",
+      description:"Learn how to deploy a Next.js application in a production environment on an Ubuntu VPS with this comprehensive guide. Follow step-by-step instructions to set up NGINX, PM2, and configure your Next.js app for seamless hosting.",
+      author:"Jhon Doe",
+      date:"March 23, 2024"
+    },
+  ]
   return (
     <div  className='h-screen'>
       <div className='text-2xl text-left font-medium'>
