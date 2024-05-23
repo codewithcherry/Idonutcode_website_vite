9d3f49fe9a79c215edf53191333bc060001874f5
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-23 14:56:32 +0530

    create a Route in app.jsx for routing of about page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Settings from './Components/Settings Components/Settings'
 import { Routes,Route, Outlet } from 'react-router-dom'
 import CoursePage from './Components/Courses/CoursePage'
 import Blogs from './Components/Blogs/Blogs'
+import AboutUs from './Components/About/AboutUs'
 
 
 
@@ -28,6 +29,7 @@ function App() {
            <Route path='viewcourse' element={<ViewCourse />} />
         </Route>
         <Route path='/blogs' element={<Blogs />} />
+        <Route path='/about' element={<AboutUs />} />
         <Route path='/profile' element={ <Profile />} />
         <Route path='/settings' element={ <Settings />} />
       </Routes>
