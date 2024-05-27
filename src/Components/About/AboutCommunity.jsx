964a988ee1e9e428f2b996b80bb24918c341a51b
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-27 20:19:14 +0530

    style the aboutCommunity component for About page

--- src/Components/About/AboutCommunity.jsx
+++ src/Components/About/AboutCommunity.jsx
@@ -3,64 +3,64 @@ import React from 'react'
 const AboutCommunity = () => {
   return (
     <div>
-      <div>
-        <div>
-            <h1>Our Community</h1>
+      <div className='my-6'>
+        <div className='m-2 p-3'>
+            <h1 className='text-center text-2xl font-medium underline underline-offset-8'>Our Community</h1>
         </div>
-        <div>
-            <div>
-                <div>
-                    <div>
+        <div className='flex justify-center w-full my-6'>
+            <div className='grid grid-cols-3 place-items-center gap-4 w-full'>
+                <div className=' text-center m-2 p-2 bg-white shadow-md rounded-md w-2/3'>
+                    <div className='flex justify-center'>
                         <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/previews/018/930/413/non_2x/instagram-logo-instagram-icon-transparent-free-png.png" alt="" />
                     </div>
                     <div>
-                        <h1>10k+</h1>
-                        <p>Followers</p>
+                        <h1 className='text-2xl text-slate-600 font-medium p-2 m-2'>10k+</h1>
+                        <p className='text-lg text-slate-500 font-medium'>Followers on Instagram</p>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className='text-center m-2 p-2 bg-white shadow-md rounded-md w-2/3'>
+                    <div className='flex justify-center'>
                         <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/previews/018/930/572/non_2x/youtube-logo-youtube-icon-transparent-free-png.png" alt="" />
                     </div>
                     <div>
-                        <h1>20K+</h1>
-                        <p>Subscribers</p>
+                        <h1 className='text-2xl text-slate-600 font-medium p-2 m-2'>20K+</h1>
+                        <p className='text-lg text-slate-500 font-medium'>Subscribers on youtube</p>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className='text-center m-2 p-2 bg-white shadow-md rounded-md w-2/3'>
+                    <div className='flex justify-center'>
                         <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/thumbnails/008/845/832/small/illustration-student-raises-his-hand-png.png" alt="" />
                     </div>
                     <div>
-                        <h1>1k+</h1>
-                        <p>Students</p>
+                        <h1 className='text-2xl text-slate-600 font-medium p-2 m-2'>1k+</h1>
+                        <p className='text-lg text-slate-500 font-medium'>Currently enrolled students</p>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className='text-center m-2 p-2 bg-white shadow-md rounded-md w-2/3'>
+                    <div className='flex justify-center'>
                         <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/previews/021/013/584/original/graduation-hat-on-transparent-background-free-png.png" alt="" />
                     </div>
                     <div>
-                        <h1>530+</h1>
-                        <p>Graduates</p>
+                        <h1 className='text-2xl text-slate-600 font-medium p-2 m-2'>530+</h1>
+                        <p className='text-lg text-slate-500 font-medium'>Students are Graduated </p>
                     </div>
                 </div>
-                <div>
-                <div>
+                <div className='text-center m-2 p-2 bg-white shadow-md rounded-md w-2/3'>
+                    <div className='flex justify-center'>
                         <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/previews/019/879/208/original/video-camera-symbol-video-camera-icon-symbol-illustration-on-transparent-background-free-png.png" alt="" />
                     </div>
                     <div>
-                        <h1>2k+</h1>
-                        <p>Videos</p>
+                        <h1 className='text-2xl text-slate-600 font-medium p-2 m-2'>2k+</h1>
+                        <p className='text-lg text-slate-500 font-medium'>Course Videos</p>
                     </div>
                 </div>
-                <div>
-                <div>
-                        <img className='h-32 w-32' src="https://www.freeiconspng.com/thumbs/training-icon/leadership-training-icon-1.png" alt="" />
+                <div className='text-center m-2 p-2 bg-white shadow-md rounded-md w-2/3'>
+                    <div className='flex justify-center'>
+                        <img className='h-32 w-32  ' src="https://www.freeiconspng.com/thumbs/training-icon/leadership-training-icon-1.png" alt="" />
                     </div>
                     <div>
-                        <h1>100+</h1>
-                        <p>Instructors</p>
+                        <h1 className='text-2xl text-slate-600 font-medium p-2 m-2'>100+</h1>
+                        <p className='text-lg text-slate-500 font-medium'>Instructors lead content</p>
                     </div>
                 </div>
             </div>
