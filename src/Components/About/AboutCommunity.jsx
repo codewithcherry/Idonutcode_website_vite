50196158b6504e17461162f3db5fa2c90f49a3a9
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-27 19:27:21 +0530

    added the UI and elements and designed cards for community component

--- src/Components/About/AboutCommunity.jsx
+++ src/Components/About/AboutCommunity.jsx
@@ -7,6 +7,64 @@ const AboutCommunity = () => {
         <div>
             <h1>Our Community</h1>
         </div>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/previews/018/930/413/non_2x/instagram-logo-instagram-icon-transparent-free-png.png" alt="" />
+                    </div>
+                    <div>
+                        <h1>10k+</h1>
+                        <p>Followers</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/previews/018/930/572/non_2x/youtube-logo-youtube-icon-transparent-free-png.png" alt="" />
+                    </div>
+                    <div>
+                        <h1>20K+</h1>
+                        <p>Subscribers</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/thumbnails/008/845/832/small/illustration-student-raises-his-hand-png.png" alt="" />
+                    </div>
+                    <div>
+                        <h1>1k+</h1>
+                        <p>Students</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/previews/021/013/584/original/graduation-hat-on-transparent-background-free-png.png" alt="" />
+                    </div>
+                    <div>
+                        <h1>530+</h1>
+                        <p>Graduates</p>
+                    </div>
+                </div>
+                <div>
+                <div>
+                        <img className='h-32 w-32' src="https://static.vecteezy.com/system/resources/previews/019/879/208/original/video-camera-symbol-video-camera-icon-symbol-illustration-on-transparent-background-free-png.png" alt="" />
+                    </div>
+                    <div>
+                        <h1>2k+</h1>
+                        <p>Videos</p>
+                    </div>
+                </div>
+                <div>
+                <div>
+                        <img className='h-32 w-32' src="https://www.freeiconspng.com/thumbs/training-icon/leadership-training-icon-1.png" alt="" />
+                    </div>
+                    <div>
+                        <h1>100+</h1>
+                        <p>Instructors</p>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   )
