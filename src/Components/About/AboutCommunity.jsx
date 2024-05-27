2b349c705c7aaaea49f40469a9abe168c498a2e7
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-27 19:03:41 +0530

    add the aboutcoummnity to main component for developoment

---
+++ src/Components/About/AboutCommunity.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutCommunity = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <h1>Our Community</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutCommunity
--- src/Components/About/AboutUs.jsx
+++ src/Components/About/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutCompany from './AboutCompany'
+import AboutCommunity from './AboutCommunity'
 
 const AboutUs = () => {
   return (
     <div>
       <AboutCompany />
+      <AboutCommunity />
     </div>
   )
 }
