4a1977414610430c9a496b85f517d4fd933653ad
Prashanth Naroju <prashanth.naro@gmail.com>
2024-05-31 22:22:32 +0530

    style the contact us component

--- src/Components/About/ContactUs.jsx
+++ src/Components/About/ContactUs.jsx
@@ -2,52 +2,46 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div>
-      <div>
-        <div>
+    <div className='my-6'>
+      <div className='flex justify-center'>
+        <div className='flex justify-center w-[80%] items-center bg-slate-100 p-2 rounded-lg shadow-lg py-4'>
             <div>
-                <h1>
+                <h1 className='text-2xl text-black font-medium m-2 p-2'>
                     Contact Us
                 </h1>
-                <p>
+                <p className='text-md m-2 p-2 text-slate-600'>
                     Need to get in touch with us? Either fill out the form with your enquiry  or find the customer help emails, you'd like contact below.
                 </p>
             </div>
-            <div>
+            <div className='shadow-md p-2 bg-white rounded-lg '>
                     <div>
                         <form >
-                           <div>
-                                <label htmlFor="firstname">
-                                    First Name
-                                </label>
-                                <input type="text" placeholder='First Name' />
+                           <div className='flex'>
+                                <div>
+                                        
+                                        <input type="text" placeholder='First Name' className='m-2 p-2 rounded-md shadow-md outline outline-2 outline-blue-400 focus:outline-green-400' />
+                                </div>
+                                <div>
+                                      
+                                        <input type="text" placeholder='Last Name' className='m-2 p-2 rounded-md shadow-md outline outline-2 outline-blue-400 focus:outline-green-400' />
+                                </div>
                            </div>
                            <div>
-                                <label htmlFor="lastName">
-                                    Last Name
-                                </label>
-                                <input type="text" placeholder='Last Name' />
+                                
+                                <input type="email" placeholder='enter your email id'  className='m-2 p-2 rounded-md shadow-md outline outline-2 outline-blue-400 focus:outline-green-400'/>
                            </div>
                            <div>
-                                <label htmlFor="emailid">
-                                    Email
-                                </label>
-                                <input type="email" placeholder='enter your email id' />
+                                
+                                <input type="phone" placeholder='+XX XXX-XXX-0000' className='m-2 p-2 rounded-md shadow-md outline outline-2 outline-blue-400 focus:outline-green-400'/>
                            </div>
                            <div>
-                                <label htmlFor="phonenumber">
-                                    Phone
-                                </label>
-                                <input type="phone" placeholder='+XX XXX-XXX-0000'/>
-                           </div>
-                           <div>
-                                <label htmlFor="inquiry">
+                                <label htmlFor="inquiry" className='m-2 text-sm text-slate-600'>
                                     what can we help you with?
                                 </label>
-                                <textarea name="inquiry" id="inquiry" cols={50} rows={10} placeholder='Type your inquiry here....'></textarea>
+                                <textarea name="inquiry" id="inquiry" cols={50} rows={5} placeholder='Type your inquiry here....' className='m-2 p-2 rounded-md shadow-md outline outline-2 outline-blue-400 focus:outline-green-400'></textarea>
                            </div>
                            <div>
-                            <input type="submit" value={"Submit"} />
+                            <input type="submit" value={"Submit"} className='p-2 m-2 bg-green-300 rounded-lg text-white font-bold hover:bg-green-400' />
                            </div>
                         </form>
                     </div>
