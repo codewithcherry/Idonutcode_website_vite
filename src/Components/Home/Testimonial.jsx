c94701f9233cb0dc463e096a02d881ce9cd80286
Prashanth Naroju <prashanth.naro@gmail.com>
2024-06-07 13:31:45 +0530

    change the testimonial card elements to make more aesthetic

--- src/Components/Home/Testimonial.jsx
+++ src/Components/Home/Testimonial.jsx
@@ -35,13 +35,20 @@ const Testimonial = () => {
                         return (
                             <div key={index} className='w-1/4 mx-4 p-4 bg-white shadow-2xl rounded-lg'> 
                             <span>
-                            <svg width="64px" height="64px" viewBox="-3 -3 26.00 26.00" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.04"></g><g id="SVGRepo_iconCarrier"> <title>quotes [#111]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke-width="0.0002" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-340.000000, -7723.000000)" fill="#7d7d7c"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M296,7563 L296,7571 L300,7571 L300,7575 L304,7575 L304,7563 L296,7563 Z M284,7563 L292,7563 L292,7575 L288,7575 L288,7571 L284,7571 L284,7563 Z" id="quotes-[#111]"> </path> </g> </g> </g> </g></svg>  
+                            <svg width="64px" height="64px" viewBox="-3.36 -3.36 30.72 30.72" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path fill="none" d="M0 0h24v24H0z"></path> <path d="M4.583 17.321C3.553 16.227 3 15 3 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179zm10 0C13.553 16.227 13 15 13 13.011c0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1-3.5 3.5c-1.073 0-2.099-.49-2.748-1.179z"></path> </g> </g></svg>
                             </span>
                                 <h2 className='text-center text-lg text-slate-600'>"{item.title}"</h2>
                                 <p className='text-md text-slate-400'>{item.desscription}</p>
+                                <div >
+                                    <div>
+                                        <p className='text-right text-lg text-slate-600 font-semibold'>{item.userName}</p>
+                                        <p className='text-right text-sm text-slate-500'>{item.position}</p>
+                                    </div>
+                                    <div>
+                                        <img className='h-12 w-12 rounded-full shadow-md' src="https://www.befunky.com/images/wp/wp-2021-01-linkedin-profile-picture-after.jpg?auto=avif,webp&format=jpg&width=944" alt="" />
+                                    </div>
+                                </div>
                                 
-                                <p className='text-right text-lg text-slate-600 font-semibold'>{item.userName}</p>
-                                <p className='text-right text-sm text-slate-500'>{item.position}</p>
                             </div>
                         )
                 })
